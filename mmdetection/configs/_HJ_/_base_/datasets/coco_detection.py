a1a9de06ca5ed68ad76b60cdb60be5d7ce2a6241
SS-hj <gkwjd111@naver.com>
2023-06-06 02:09:26 +0000

    [Feat] hard augmentation

--- mmdetection/configs/_HJ_/_base_/datasets/coco_detection.py
+++ mmdetection/configs/_HJ_/_base_/datasets/coco_detection.py
@@ -14,43 +14,89 @@ classes = ('chevrolet_malibu_sedan_2012_2016', 'chevrolet_malibu_sedan_2017_2019
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+multi_scale = [(800, 1333),(1600, 2666)]
+
+albu_train_transforms = [
+        dict(type='RandomCrop', height=500, width=300, p=0.3),
+		dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Blur', blur_limit=3, p=1.0),
+            dict(type='MotionBlur', blur_limit=11, p=1.0),
+            dict(type='MedianBlur', blur_limit=3, p=1.0),
+        ],
+        p=0.5),
+		dict(
+        type='OneOf',
+        transforms=[
+            dict(type='ISONoise', p=1.0),
+            dict(type='RandomGamma', p=1.0),
+            dict(type='HueSaturationValue', p=1.0),
+            dict(type='ChannelShuffle', p=1.0),
+            dict(type='RGBShift', p=1.0),
+            dict(type='CLAHE', clip_limit=(1, 10), p=1.0),
+            dict(type='RandomBrightnessContrast', p=1.0), 
+        ],
+        p=0.6),
+]
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', img_scale=multi_scale, multiscale_mode='range', keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(
+        type='Albu',
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap={
+            'img': 'image',
+            'gt_bboxes': 'bboxes'
+        },
+        update_pad_shape=False,
+        skip_img_without_anno=True),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
+
 val_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
-        type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+    type='MultiScaleFlipAug',
+        img_scale=multi_scale,
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', multiscale_mode='range', keep_ratio=True),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
+
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1333, 800),
+        img_scale=multi_scale,
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', multiscale_mode='range', keep_ratio=True),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
+
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
@@ -62,12 +108,13 @@ data = dict(
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'K-fold_val1.json',
+        ann_file=data_root + 'augmented_val.json',
         img_prefix=data_root,
         classes=classes,
         pipeline=val_pipeline),
     test=dict(
         type=dataset_type,
+        ann_file=data_root + 'annotations/test.json',
         img_prefix=data_root,
         pipeline=test_pipeline))
-evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP_75')
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP')
--- mmdetection/configs/_HJ_/cascade-mask-rcnn_convnext-t.py
+++ mmdetection/configs/_HJ_/cascade-mask-rcnn_convnext-t.py
@@ -81,53 +81,6 @@ model = dict(
             loss_bbox=dict(type='GIoULoss', loss_weight=10.0))
     ]))
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
-# augmentation strategy originates from DETR / Sparse RCNN
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(
-        type='AutoAugment',
-        policies=[[
-            dict(
-                type='Resize',
-                img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
-                           (608, 1333), (640, 1333), (672, 1333), (704, 1333),
-                           (736, 1333), (768, 1333), (800, 1333)],
-                multiscale_mode='value',
-                keep_ratio=True)
-        ],
-                  [
-                      dict(
-                          type='Resize',
-                          img_scale=[(400, 1333), (500, 1333), (600, 1333)],
-                          multiscale_mode='value',
-                          keep_ratio=True),
-                      dict(
-                          type='RandomCrop',
-                          crop_type='absolute_range',
-                          crop_size=(384, 600),
-                          allow_negative_crop=True),
-                      dict(
-                          type='Resize',
-                          img_scale=[(480, 1333), (512, 1333), (544, 1333),
-                                     (576, 1333), (608, 1333), (640, 1333),
-                                     (672, 1333), (704, 1333), (736, 1333),
-                                     (768, 1333), (800, 1333)],
-                          multiscale_mode='value',
-                          override=True,
-                          keep_ratio=True)
-                  ]]),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
-data = dict(train=dict(pipeline=train_pipeline), persistent_workers=True)
-
 optimizer = dict(
     _delete_=True,
     constructor='LearningRateDecayOptimizerConstructor',
@@ -142,7 +95,7 @@ optimizer = dict(
     })
 
 lr_config = dict(warmup_iters=1000, step=[27, 33])
-runner = dict(max_epochs=36)
+runner = dict(max_epochs=20)
 
 # you need to set mode='dynamic' if you are using pytorch<=1.5.0
 fp16 = dict(loss_scale=dict(init_scale=512))
