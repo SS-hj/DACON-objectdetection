381d7de1486ab3952afc1a1393a053e724bacfe1
SS-hj <gkwjd111@naver.com>
2023-06-22 17:54:28 +0000

    [Add] deterministic to train.py

--- mmdetection/tools/train.py
+++ mmdetection/tools/train.py
@@ -61,6 +61,7 @@ def parse_args():
     parser.add_argument(
         '--deterministic',
         action='store_true',
+        default=True,
         help='whether to set deterministic options for CUDNN backend.')
     parser.add_argument(
         '--options',
